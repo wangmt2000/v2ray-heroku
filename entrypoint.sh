8df49bfc04fa290c78915676656afe8f82334c6b
wangmt2000 <32374305+wangmt2000@users.noreply.github.com>
2019-03-25 15:30:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 cd /v2raybin
-if [ "$VER" = "latest" ]; then
-  wget http://storage.googleapis.com/v2ray-docker/v2ray 
-  wget http://storage.googleapis.com/v2ray-docker/v2ctl
-  wget http://storage.googleapis.com/v2ray-docker/geoip.dat
-  wget http://storage.googleapis.com/v2ray-docker/geosite.dat
-else
+#if [ "$VER" = "latest" ]; then
+ # wget http://storage.googleapis.com/v2ray-docker/v2ray 
+  #wget http://storage.googleapis.com/v2ray-docker/v2ctl
+  #wget http://storage.googleapis.com/v2ray-docker/geoip.dat
+  #wget http://storage.googleapis.com/v2ray-docker/geosite.dat
+#else
   wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
   unzip v2ray.zip 
   mv /v2raybin/v2ray-v$VER-linux-64/v2ray .
   mv /v2raybin/v2ray-v$VER-linux-64/v2ctl .
   mv /v2raybin/v2ray-v$VER-linux-64/geoip.dat .
   mv /v2raybin/v2ray-v$VER-linux-64/geosite.dat .
-fi
+#fi
 
 chmod +x v2ray
 chmod +x v2ctl
