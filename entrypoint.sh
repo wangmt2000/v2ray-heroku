ff66ba8ef7c6128b7eaa314032fdd9ca25ffbca0
wangmt2000 <32374305+wangmt2000@users.noreply.github.com>
2019-04-03 21:58:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,15 +14,16 @@
 #chmod -R g+rwX /v2raybin
 
 cd /v2raybin
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
+#echo -e -n "$CONFIG_JSON1" > config.json
+#echo -e -n "$PORT" >> config.json
+#echo -e -n "$CONFIG_JSON2" >> config.json
+#echo -e -n "$UUID" >> config.json
+#echo -e -n "$CONFIG_JSON3" >> config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
 fi
+echo  "$PORT"
 ./v2ray
 
 
